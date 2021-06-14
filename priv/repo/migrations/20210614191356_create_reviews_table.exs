08f9838aaa8214e803a9552d22622a8aac1763fc
Hugo <hugolacerdar@gmail.com>
2021-06-14 18:35:54 -0300

    Add review table migration

---
+++ priv/repo/migrations/20210614191356_create_reviews_table.exs
@@ -0,0 +1,18 @@
+defmodule ReviewIt.Repo.Migrations.CreateReviewsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:reviews) do
+      add :user_id, references(:users, on_delete: :delete_all)
+      add :post_id, references(:posts, on_delete: :delete_all)
+      add :description, :text
+      add :sugestions, :text
+      add :strengths, :text
+      add :weakness, :text
+
+      timestamps()
+    end
+
+    create unique_index(:reviews, [:user_id, :post_id])
+  end
+end
