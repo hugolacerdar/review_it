1a026ce0f87068e6b010ffe791eb4646c81b718a
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-14 19:27:00 -0300

    Use ex_machina

--- mix.exs
+++ mix.exs
@@ -52,7 +52,8 @@ defmodule ReviewIt.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
-      {:pbkdf2_elixir, "~> 1.3"}
+      {:pbkdf2_elixir, "~> 1.3"},
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
 
