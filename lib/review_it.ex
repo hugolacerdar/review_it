f2d3d47fea39c355904f7c0b4e16af574845e794
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-14 20:21:03 -0300

    Add get user by id to ReviewIt context

--- lib/review_it.ex
+++ lib/review_it.ex
@@ -6,4 +6,7 @@ defmodule ReviewIt do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias ReviewIt.Users.Get, as: UserGet
+
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
