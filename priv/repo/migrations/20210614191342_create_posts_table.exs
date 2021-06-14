ce68c070598690215192e7847fe577b3f4d9cb40
Hugo <hugolacerdar@gmail.com>
2021-06-14 18:35:13 -0300

    Add post table migration

---
+++ priv/repo/migrations/20210614191342_create_posts_table.exs
@@ -0,0 +1,15 @@
+defmodule ReviewIt.Repo.Migrations.CreatePostsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :title, :string
+      add :description, :string
+      add :code_url, :string
+      add :creator_id, references(:users, on_delete: :delete_all)
+      add :reviwer_id, references(:users, on_delete: :delete_all)
+
+      timestamps()
+    end
+  end
+end
