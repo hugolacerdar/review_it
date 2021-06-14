4186188f78340c1ef14704898d69070cd9082841
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-14 20:34:31 -0300

    Add authenticate method to guardian

--- lib/review_it_web/auth/guardian.ex
+++ lib/review_it_web/auth/guardian.ex
@@ -1,7 +1,7 @@
 defmodule ReviewItWeb.Auth.Guardian do
   use Guardian, otp_app: :review_it
 
-  alias ReviewIt.User
+  alias ReviewIt.{Error, User}
 
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
 
@@ -9,5 +9,19 @@ defmodule ReviewItWeb.Auth.Guardian do
     claims
     |> Map.get("sub")
     |> ReviewIt.get_user_by_id()
+  end
+
+  def authenticate(%{"email" => email, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- ReviewIt.get_user_by_email(email),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, token, user}
+    else
+      _ -> {:error, Error.build(:unauthorized, "Please verify your credentials")}
+    end
+  end
+
+  def authenticate(_params) do
+    {:error, Error.build(:bad_request, "Invalid or missing params")}
   end
 end
