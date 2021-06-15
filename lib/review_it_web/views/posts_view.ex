0863e996d4025b5ee7157444d05873dc54486d32
Hugo <hugolacerdar@gmail.com>
2021-06-15 19:38:03 -0300

    Add Post views module

---
+++ lib/review_it_web/views/posts_view.ex
@@ -0,0 +1,14 @@
+defmodule ReviewItWeb.PostsView do
+  use ReviewItWeb, :view
+  alias ReviewIt.Post
+
+  def render("create.json", %{post: %Post{} = post}) do
+    %{
+      message: "Post created!",
+      post: post
+    }
+  end
+
+  def render("post.json", %{post: %Post{} = post}), do: %{post: post}
+  def render("show_all.json", %{result: result}), do: %{result: result}
+end
