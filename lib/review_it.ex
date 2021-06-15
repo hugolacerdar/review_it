b1ab5ab0f51c56c1841893a39045e5293d1b1197
Hugo <hugolacerdar@gmail.com>
2021-06-15 19:41:59 -0300

    Add Post related facades

--- lib/review_it.ex
+++ lib/review_it.ex
@@ -6,10 +6,16 @@ defmodule ReviewIt do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias ReviewIt.Posts.Create, as: PostCreate
+  alias ReviewIt.Posts.Get, as: PostGet
   alias ReviewIt.Users.Create, as: UserCreate
   alias ReviewIt.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
   defdelegate get_user_by_email(email), to: UserGet, as: :by_email
+
+  defdelegate create_post(params), to: PostCreate, as: :call
+  defdelegate get_post_by_creator_id(id), to: PostGet, as: :by_creator_id
+  defdelegate get_post_by_reviewer_id(id), to: PostGet, as: :by_reviewer_id
 end
