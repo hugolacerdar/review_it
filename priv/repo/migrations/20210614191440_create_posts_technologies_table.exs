5c55b04280934377f25e838051e1211793c355c4
Hugo <hugolacerdar@gmail.com>
2021-06-16 19:35:59 -0300

    Update tech_id to technology_id

--- priv/repo/migrations/20210614191440_create_posts_technologies_table.exs
+++ priv/repo/migrations/20210614191440_create_posts_technologies_table.exs
@@ -2,13 +2,11 @@ defmodule ReviewIt.Repo.Migrations.CreatePostsTechnologiesTable do
   use Ecto.Migration
 
   def change do
-    create table(:posts_technologies) do
+    create table(:posts_technologies, primary_key: false) do
       add :post_id, references(:posts, on_delete: :delete_all)
-      add :tech_id, references(:technologies, on_delete: :delete_all)
-
-      timestamps()
+      add :technology_id, references(:technologies, on_delete: :delete_all)
     end
 
-    create unique_index(:posts_technologies, [:post_id, :tech_id])
+    create unique_index(:posts_technologies, [:post_id, :technology_id])
   end
 end
