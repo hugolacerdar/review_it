55c763a459b22c6349e00df5e47abdaaad8b2816
Hugo <hugolacerdar@gmail.com>
2021-06-16 11:35:45 -0300

    Fix comma

--- priv/repo/migrations/20210614191342_create_posts_table.exs
+++ priv/repo/migrations/20210614191342_create_posts_table.exs
@@ -6,7 +6,7 @@ defmodule ReviewIt.Repo.Migrations.CreatePostsTable do
       add :title, :string, null: false
       add :description, :string, null: false
       add :code_url, :string, null: false
-      add :creator_id, references(:users, on_delete: :delete_all),
+      add :creator_id, references(:users, on_delete: :delete_all)
       add :reviewer_id, references(:users, on_delete: :delete_all)
 
       timestamps()
