ee18f66cb9b6537b5814d09fde121fdebcc7e5f6
Hugo <hugolacerdar@gmail.com>
2021-06-16 19:30:58 -0300

    Update post creatito insert technologies associaion

--- lib/review_it/posts/create.ex
+++ lib/review_it/posts/create.ex
@@ -1,24 +1,27 @@
 defmodule ReviewIt.Posts.Create do
   alias Ecto.Changeset
-  alias ReviewIt.{Error, Post, Repo, User}
+  alias ReviewIt.{Error, Post, Repo, Technology}
 
-  def call(params) do
-    with %Changeset{valid?: true} <- Post.changeset(params),
-         creator_id <- Map.get(params, "creator_id"),
-         {:ok, %User{}} <-
-           ReviewIt.get_user_by_id(creator_id) do
-      params
-      |> Post.changeset()
+  def call(%{"technologies" => technologies} = params) do
+    technologies =
+      technologies
+      |> Stream.uniq()
+      |> Enum.map(&Repo.get(Technology, &1))
+
+    with true <- Enum.all?(technologies),
+         %Changeset{valid?: true} = changeset <- Post.changeset(params, technologies) do
+      changeset
       |> Repo.insert()
       |> handle_insert()
     else
       {:error, _} -> {:error, Error.build_user_not_found_error()}
       %Changeset{valid?: false} = changeset -> {:error, Error.build_changeset_error(changeset)}
+      false -> {:error, Error.build(:not_found, "Invalid technology id")}
     end
   end
 
   defp handle_insert({:ok, %Post{} = post}) do
-    post = Repo.preload(post, :author)
+    post = Repo.preload(post, [:author, :technologies])
 
     {:ok, post}
   end
