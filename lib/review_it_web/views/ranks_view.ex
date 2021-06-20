5215a58d8648ffbe8b5b03f68faeb504cacc0cf0
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-20 02:56:22 -0300

    Add view to render single rank

--- lib/review_it_web/views/ranks_view.ex
+++ lib/review_it_web/views/ranks_view.ex
@@ -2,4 +2,5 @@ defmodule ReviewItWeb.RanksView do
   use ReviewItWeb, :view
 
   def render("index.json", %{ranks: ranks}), do: %{ranks: ranks}
+  def render("rank.json", %{rank: rank}), do: %{rank: rank}
 end
--- test/review_it_web/views/ranks_view_test.exs
+++ test/review_it_web/views/ranks_view_test.exs
@@ -26,4 +26,22 @@ defmodule ReviewItWeb.RanksViewTest do
              ]
            } = response
   end
+
+  test "renders rank.json" do
+    # Arrange
+    rank = build(:rank)
+
+    # Act
+    response = render(RanksView, "rank.json", rank: rank)
+
+    # Assert
+    assert %{
+             rank: %Rank{
+               month: _,
+               year: _,
+               score: _,
+               user_id: _
+             }
+           } = response
+  end
 end
