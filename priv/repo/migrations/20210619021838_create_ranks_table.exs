23689f389fcfa82547d452a07aae3fc968431183
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-20 15:36:50 -0300

    Fix formatting

--- priv/repo/migrations/20210619021838_create_ranks_table.exs
+++ priv/repo/migrations/20210619021838_create_ranks_table.exs
@@ -2,7 +2,7 @@ defmodule ReviewIt.Repo.Migrations.CreateRanksTable do
   use Ecto.Migration
 
   def change do
-    create table :ranks do
+    create table(:ranks) do
       add :user_id, references(:users, on_delete: :delete_all)
       add :month, :integer
       add :year, :integer
