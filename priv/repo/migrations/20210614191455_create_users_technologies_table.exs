63fda2595ef1de40511222d3dd2b0a1542e14bb0
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-20 00:28:43 -0300

    Fix users technologies table migration

--- priv/repo/migrations/20210614191455_create_users_technologies_table.exs
+++ priv/repo/migrations/20210614191455_create_users_technologies_table.exs
@@ -2,11 +2,9 @@ defmodule ReviewIt.Repo.Migrations.CreateUsersTechnologiesTable do
   use Ecto.Migration
 
   def change do
-    create table(:users_technologies) do
+    create table(:users_technologies, primary_key: false) do
       add :user_id, references(:users, on_delete: :delete_all)
       add :technology_id, references(:technologies, on_delete: :delete_all)
-
-      timestamps()
     end
 
     create unique_index(:users_technologies, [:user_id, :technology_id])
