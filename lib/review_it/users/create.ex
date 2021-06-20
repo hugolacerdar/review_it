bf345c9d483591af1ea3fb8747ac7e292de1a46e
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-20 00:35:56 -0300

    Update user to save techs on create

--- lib/review_it/users/create.ex
+++ lib/review_it/users/create.ex
@@ -1,11 +1,43 @@
 defmodule ReviewIt.Users.Create do
-  alias ReviewIt.{Error, Repo, User}
+  import Ecto.Changeset, only: [add_error: 3, put_assoc: 3]
+
+  alias ReviewIt.{Error, Repo, Technology, User}
 
   def call(params) do
     params
     |> User.changeset()
+    |> handle_expert_technologies(params)
     |> Repo.insert()
     |> handle_insert()
+  end
+
+  defp handle_expert_technologies(changeset, %{"is_expert" => true} = params) do
+    params
+    |> Map.get("technologies")
+    |> handle_technologies(changeset)
+  end
+
+  defp handle_expert_technologies(changeset, _params), do: changeset
+
+  defp handle_technologies([_t | _h] = technologies, changeset) do
+    technologies =
+      technologies
+      |> Stream.uniq()
+      |> Enum.map(&Repo.get(Technology, &1))
+
+    if Enum.all?(technologies) do
+      put_assoc(changeset, :technologies, technologies)
+    else
+      add_error(changeset, :technologies, "Technology not found")
+    end
+  end
+
+  defp handle_technologies(nil, changeset) do
+    add_error(
+      changeset,
+      :technologies,
+      "When user is expert a technologies list must be provided"
+    )
   end
 
   defp handle_insert({:ok, %User{}} = result), do: result
--- test/review_it/users/create_test.exs
+++ test/review_it/users/create_test.exs
@@ -67,5 +67,70 @@ defmodule ReviewIt.Users.CreateTest do
 
       assert errors_on(changeset) == expected_response
     end
+
+    test "when create an expert, returns the user" do
+      # Arrange
+      %{id: tech_id} = insert(:technology)
+
+      params =
+        build(:user_params, %{
+          "is_expert" => true,
+          "technologies" => [tech_id]
+        })
+
+      # Act
+      response = Create.call(params)
+
+      # Assert
+      assert {:ok,
+              %User{
+                id: _id,
+                nickname: "Banana",
+                email: "banana@mail.com",
+                is_expert: true,
+                picture_url: nil,
+                github_url: "https://github.com/banana",
+                linkedin_url: "https://linkedin.com/in/banana",
+                score: 0
+              }} = response
+    end
+
+    test "when create an expert and not found the technology, returns an error" do
+      # Arrange
+      tech_id = "3beb6a91-1580-455a-b377-13930f08565d"
+
+      params =
+        build(:user_params, %{
+          "is_expert" => true,
+          "technologies" => [tech_id]
+        })
+
+      # Act
+      response = Create.call(params)
+
+      # Assert
+      assert {:error, %Error{status: :bad_request, result: changeset}} = response
+
+      expected_response = %{technologies: ["Technology not found"]}
+
+      assert errors_on(changeset) == expected_response
+    end
+
+    test "when create an expert and not provide the technology list, returns an error" do
+      # Arrange
+      params = build(:user_params, %{"is_expert" => true})
+
+      # Act
+      response = Create.call(params)
+
+      # Assert
+      assert {:error, %Error{status: :bad_request, result: changeset}} = response
+
+      expected_response = %{
+        technologies: ["When user is expert a technologies list must be provided"]
+      }
+
+      assert errors_on(changeset) == expected_response
+    end
   end
 end
