56954bc1908567c08406c1ddbbbe5d8652171686
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-20 18:11:46 -0300

    Add techs seed

---
+++ priv/repo/seeds_techs.exs
@@ -0,0 +1,78 @@
+alias ReviewIt.{Repo, Technology}
+
+%Technology{
+  hex_color: "#ac98b1",
+  name: "Elixir",
+  id: "d074e4f3-9082-4a20-8094-7f95368d54cb"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#e95122",
+  name: "Phoenix",
+  id: "eafdbd93-1bc2-4f0f-b70b-d184beb08bd1"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#7ec728",
+  name: "Node.js",
+  id: "9ffeb005-6879-45e7-8bd4-f6d572ddf61b"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#f7df1e",
+  name: "JavaScript",
+  id: "ef4e1e83-e4f0-4cd7-badc-cf410db0e9c7"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#61dafb",
+  name: "React",
+  id: "7a1badd8-ce18-4c9c-b032-1424ba07efeb"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#01d1f7",
+  name: "React Native",
+  id: "47e1c5a4-309f-4b81-a9b6-2c328447bca8"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#43caf6",
+  name: "Flutter",
+  id: "39cc57e2-deee-4298-8a31-09393eda1e9c"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#325d87",
+  name: "SQL",
+  id: "f16e20db-6ff8-424c-93d1-59b146abe758"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#4aa348",
+  name: "MongoDB",
+  id: "63cb3232-1571-4cfb-af04-6f54fb944734"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#ec501e",
+  name: "HTML5",
+  id: "62ee1f3f-2339-41db-9434-fd0afa2d3521"
+}
+|> Repo.insert!()
+
+%Technology{
+  hex_color: "#409ad6",
+  name: "CSS3",
+  id: "55587a87-5dac-4aa8-9df2-b7c4689d25e4"
+}
+|> Repo.insert!()
