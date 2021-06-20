3e9f62b1fb14d0694ea962fe706e6c7332cadc9f
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-06-20 02:30:28 -0300

    Add method to get user rank

--- lib/review_it/ranks/get.ex
+++ lib/review_it/ranks/get.ex
@@ -1,7 +1,7 @@
 defmodule ReviewIt.Ranks.Get do
   import Ecto.Query
 
-  alias ReviewIt.{Repo, Rank}
+  alias ReviewIt.{Error, Repo, Rank}
 
   @default_limit 10
 
@@ -21,5 +21,27 @@ defmodule ReviewIt.Ranks.Get do
         preload: [:user]
 
     Repo.all(query)
+  end
+
+  def by_user_id(%{"id" => user_id} = params) do
+    %{month: month, year: year} = DateTime.utc_now()
+
+    month = Map.get(params, "month", month)
+    year = Map.get(params, "year", year)
+
+    ranked_query =
+      from r in Rank,
+        where: r.month == ^month and r.year == ^year,
+        select: %{r | position: dense_rank() |> over(order_by: [desc: r.score])}
+
+    query =
+      from r in subquery(ranked_query),
+        where: r.user_id == ^user_id,
+        preload: [:user]
+
+    case Repo.one(query) do
+      nil -> {:error, Error.build(:not_found, "Rank not found")}
+      rank -> {:ok, rank}
+    end
   end
 end
--- test/review_it/ranks/get_test.exs
+++ test/review_it/ranks/get_test.exs
@@ -3,7 +3,7 @@ defmodule ReviewIt.Ranks.GetTest do
 
   import ReviewIt.Factory
 
-  alias ReviewIt.Rank
+  alias ReviewIt.{Error, Rank}
   alias ReviewIt.Ranks.Get
 
   describe "by_period/1" do
@@ -24,6 +24,40 @@ defmodule ReviewIt.Ranks.GetTest do
 
       # Assert
       assert [%Rank{score: 200, position: 1}, %Rank{score: 100, position: 2}] = response
+    end
+  end
+
+  describe "by_user_id/1" do
+    setup do
+      %{id: user_id} = insert(:user_expert)
+      %{id: other_user_id} = insert(:user, %{is_expert: true})
+
+      %{user_id: user_id, other_user_id: other_user_id}
+    end
+
+    test "when user has rank, return user's rank", %{other_user_id: other_user_id} do
+      # Arrange
+      insert(:rank, %{score: 200})
+      insert(:rank, %{user_id: other_user_id, score: 100})
+      params = %{"id" => other_user_id}
+
+      # Act
+      response = Get.by_user_id(params)
+
+      # Assert
+      assert {:ok, %Rank{score: 100, position: 2}} = response
+    end
+
+    test "when user not has a rank, return an error" do
+      # Arrange
+      params = %{"id" => "fa1ad3df-076f-4258-b606-a3afd97c76a2"}
+
+      # Act
+      response = Get.by_user_id(params)
+
+      # Assert
+      expected_response = {:error, %Error{status: :not_found, result: "Rank not found"}}
+      assert expected_response == response
     end
   end
 end
